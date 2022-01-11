be70c97a623e267822aab70543fe1c53c8aa97b8
milenalazovska <96383916+milenalazovska@users.noreply.github.com>
2022-01-11 12:46:36 +0100

    Add files via upload

---
+++ TASK14/task14.js
@@ -0,0 +1,18 @@
+let arrayAge = [24, 34, 44, 54, 64, 74];
+findThisIndex(arrayAge);
+
+function findThisIndex(array) {
+    let index = array.indexOf(59)
+    console.log(index)
+    if (array === 59) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+
+
+
+
