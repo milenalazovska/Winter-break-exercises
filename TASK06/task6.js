91d11f755d23a9d18567f4e622e40c7a16015351
milenalazovska <96383916+milenalazovska@users.noreply.github.com>
2022-01-11 12:42:46 +0100

    Add files via upload

---
+++ TASK06/task6.js
@@ -0,0 +1,11 @@
+function newFunction(string1, string2) {
+    if (string1.length == string2.length) {
+        console.log(`true`)
+    }
+    else if (string1.length != string2.length){
+        console.log(`false`)
+    }
+}
+let strings = newFunction("anja", "sara");
+let strings1 = newFunction("milena", "maja")
+let strings2 = newFunction("aleksandar", "petar")
