09f15972895e440a1fb527530481fe12dd515304
milenalazovska <96383916+milenalazovska@users.noreply.github.com>
2022-01-11 12:46:09 +0100

    Add files via upload

---
+++ TASK13/task13.js
@@ -0,0 +1,7 @@
+let myArrayReverse = [1, 2, 3, 4, 5, 6, 7, 8];
+reverseFunction(myArrayReverse);
+
+function reverseFunction(array){
+    let myNewReverseArray = array.reverse();
+    console.log(myNewReverseArray);
+}
